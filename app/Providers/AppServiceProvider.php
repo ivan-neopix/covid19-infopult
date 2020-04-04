5d9da1ad6ff3dd42a5320061595ad605fc167d01
Misa Kovic <misa@weareneopix.com>
2020-04-04 20:53:06 +0200

    feat(dates): use CarbonImmutable for dates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\View\Components\PostComponent;
+use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
+    {
+        $this->registerBladeComponents();
+
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function registerBladeComponents()
     {
         Blade::component('post', PostComponent::class);
     }
