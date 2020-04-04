360af5e8810bd702239f962bf196eca1e1b3a081
Misa Kovic <misa@weareneopix.com>
2020-04-04 19:03:22 +0200

    test(tags): implement tests for the tag index page

--- database/factories/PostsFactory.php
+++ database/factories/PostsFactory.php
@@ -3,9 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Category;
+use App\Models\Tag;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(Tag::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
     ];
---
+++ tests/Feature/Admin/TagsTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tests\Traits\AdminDomain;
+use Tests\Traits\AuthenticatedAdmin;
+
+class TagsTest extends TestCase
+{
+    use RefreshDatabase, AdminDomain, AuthenticatedAdmin;
+
+    /** @test */
+    public function admin_can_visit_the_tags_index_page()
+    {
+        $tags = collect([
+            factory(Tag::class)->create(['name' => 'assistance']),
+            factory(Tag::class)->create(['name' => 'bread']),
+            factory(Tag::class)->create(['name' => 'cheap']),
+            factory(Tag::class)->create(['name' => 'delivery']),
+        ]);
+
+
+        $response = $this->get("/tags");
+
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.tags.index');
+        $response->assertSeeInOrder($tags->pluck('name')->toArray());
+    }
+
+    /** @test */
+    public function tags_index_page_will_be_paginated()
+    {
+        $tags = factory(Tag::class, 80)->create()->sortBy('name');
+
+
+        $response = $this->get("/tags?page=2");
+
+
+        $response->assertViewIs('admin.tags.index');
+        $response->assertSeeInOrder($tags->slice(50)->pluck('name')->toArray());
+    }
+
+    /** @test */
+    public function admin_can_search_for_tags()
+    {
+        $relevantTags = collect([
+            factory(Tag::class)->create(['name' => 'volunteers_assistance']),
+            factory(Tag::class)->create(['name' => 'volunteers_belgrade']),
+            factory(Tag::class)->create(['name' => 'volunteers_help']),
+        ]);
+        $irrelevantTags = collect([
+            factory(Tag::class)->create(['name' => 'food']),
+            factory(Tag::class)->create(['name' => 'vegetables']),
+            factory(Tag::class)->create(['name' => 'bread']),
+        ]);
+
+
+        $response = $this->get("/tags?search=volunteers");
+
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.tags.index');
+        $response->assertSeeInOrder($relevantTags->pluck('name')->toArray());
+        $irrelevantTags->each(function (Tag $tag) use ($response) {
+            $response->assertDontSee($tag->name);
+        });
+
+    }
+}
