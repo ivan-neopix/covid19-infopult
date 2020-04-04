cf3aa1272ad469055146c3117f51cad1695bfcf2
Misa Kovic <misa@weareneopix.com>
2020-04-04 20:51:58 +0200

    test(posts): implement tests for the post index page

---
+++ tests/Feature/Admin/PostsTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Post;
+use Carbon\CarbonImmutable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tests\Traits\AdminDomain;
+use Tests\Traits\AuthenticatedAdmin;
+
+class PostsTest extends TestCase
+{
+    use RefreshDatabase, AdminDomain, AuthenticatedAdmin;
+
+    /** @test */
+    public function admins_can_visit_the_posts_index_page()
+    {
+        $posts = collect([
+            factory(Post::class)->create(['created_at' => CarbonImmutable::create(2019, 4, 4, 20, 30)]),
+            factory(Post::class)->create(['created_at' => CarbonImmutable::create(2019, 4, 4, 19, 30)]),
+            factory(Post::class)->create(['created_at' => CarbonImmutable::create(2019, 4, 4, 18, 30)]),
+            factory(Post::class)->create(['created_at' => CarbonImmutable::create(2019, 4, 4, 17, 30)]),
+        ]);
+
+
+        $response = $this->get("/posts");
+
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.posts.index');
+        $response->assertSeeInOrder($posts->pluck('title')->toArray());
+        $response->assertSeeInOrder($posts->pluck('description')->toArray());
+    }
+
+    /** @test */
+    public function posts_index_page_will_be_paginated()
+    {
+        $posts = factory(Post::class, 18)->create();
+
+
+        $response = $this->get("/posts?page=2");
+
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.posts.index');
+        $response->assertSeeInOrder($posts->slice(10)->pluck('title')->toArray());
+    }
+
+    /** @test */
+    public function admins_can_search_for_posts_by_title()
+    {
+        $relevantPosts = collect([
+            factory(Post::class)->create(['title' => 'Test Post 1']),
+            factory(Post::class)->create(['title' => 'Test Post 2']),
+            factory(Post::class)->create(['title' => 'Test Post 3']),
+        ]);
+        $irrelevantPosts = collect([
+            factory(Post::class)->create(['title' => 'Irrelevant Post 1']),
+            factory(Post::class)->create(['title' => 'Irrelevant Post 2']),
+            factory(Post::class)->create(['title' => 'Irrelevant Post 3']),
+        ]);
+
+
+        $response = $this->get("/posts?search=test");
+
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.posts.index');
+        $response->assertSeeInOrder($relevantPosts->pluck('title')->toArray());
+        $irrelevantPosts->each(function (Post $post) use ($response) {
+            $response->assertDontSee($post->title);
+        });
+    }
+}
