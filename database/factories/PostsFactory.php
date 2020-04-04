933f9be64e33b8788fa33f61994191e89296a172
Misa Kovic <misa@weareneopix.com>
2020-04-04 20:01:47 +0200

    feat(posts): implement a Post model

--- database/factories/PostsFactory.php
+++ database/factories/PostsFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\Tag;
 use Faker\Generator as Faker;
 
@@ -15,5 +16,18 @@ $factory->define(Category::class, function (Faker $faker) {
 $factory->define(Tag::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
+    ];
+});
+
+$factory->define(Post::class, function (Faker $faker, array $attributes) {
+    if (!array_key_exists('category_id', $attributes)) {
+        $attributes['category_id'] = factory(Category::class)->create()->id;
+    }
+
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->text,
+        'link' => $faker->url,
+        'category_id' => $attributes['category_id'],
     ];
 });
