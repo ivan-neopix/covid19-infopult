a6139e12a37b13674daeacbe5d97eb7a9176a7ca
Misa Kovic <misa@weareneopix.com>
2020-04-05 16:05:16 +0200

    feat(categories): make categories soft deletable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     function posts()
     {
         return $this->hasMany(Post::class);
