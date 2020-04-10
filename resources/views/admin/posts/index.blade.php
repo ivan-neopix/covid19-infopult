ecfe5313a24fa596b51f504802c00eb20c2c5716
Ivan Stojanovic <ivan@weareneopix.com>
2020-04-11 00:08:11 +0200

    fix(admin-posts): set aloglia keys

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -72,5 +72,10 @@
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        ALGOLIA_APP_ID = `<?php echo env('ALGOLIA_APP_ID', '') ?>`;
+        ALGOLIA_PUBLIC_SECRET = `<?php echo env('ALGOLIA_PUBLIC_SECRET', '') ?>`;
+    </script>
 @endsection
 
