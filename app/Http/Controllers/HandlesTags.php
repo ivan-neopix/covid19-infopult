f172bf99154aff17ff71e588f8c553576287d6ae
Misa Kovic <misa@weareneopix.com>
2020-04-12 15:21:26 +0200

    refactor(tags): change tag collation when assigning them to posts

--- app/Http/Controllers/HandlesTags.php
+++ app/Http/Controllers/HandlesTags.php
@@ -12,7 +12,7 @@ trait HandlesTags
     protected function prepareTagIds(Request $request): Collection
     {
         $tags = collect(explode(' ', $request->input('tags')))->map(function (string $tagName) { return mb_strtolower($tagName); })->unique();
-        
+
         $newTags = $tags->diff(Tag::whereIn('name', $tags)->pluck('name'));
         Tag::insert($newTags->map(function ($tagName) {
             return ['name' => $tagName];
@@ -22,8 +22,12 @@ trait HandlesTags
             NewTagsCreated::dispatch($newTags);
         }
 
-        Tag::whereIn('name', $newTags)->searchable();
 
-        return Tag::whereIn('name', $tags)->pluck('id');
+        $query = Tag::query();
+        $collatedTags = $tags->map(function (string $tagName) use ($query) {
+            return $query->raw("'{$tagName}' COLLATE utf8mb4_croatian_ci");
+        });
+
+        return $query->whereIn('name', $collatedTags)->pluck('id');
     }
 }
