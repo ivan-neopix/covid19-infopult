7b1172bb88ffc312e9a856c7b2d4e45a09dd6386
Makro Ilic <marko@weareneopix.com>
2020-04-23 19:31:49 +0200

    refactor(*): cleanup

--- resources/views/layouts/create.blade.php
+++ resources/views/layouts/create.blade.php
@@ -1,29 +1,9 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('partials.meta')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('page_title') | {{ config('app.name') }}</title>
-
-    <link rel="preconnect dns-prefetch" href="https://www.googletagmanager.com">
-    {{--  TODO Add meta tags   --}}
-
-    <!-- Styles -->
     <link href="{{ asset('css/create.css') }}" rel="stylesheet">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo env("GOOGLE_ANALYTICS_ID", "") ?>"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '<?php echo env("GOOGLE_ANALYTICS_ID", "") ?>');
-    </script>
 </head>
 <body class="@yield('body_class')">
     <header class="site-header">
--- resources/views/web/posts/index.blade.php
+++ resources/views/web/posts/index.blade.php
@@ -45,58 +45,6 @@
         {{ $posts->links() }}
     </div>
 
-{{--    <div class="container">--}}
-{{--        <div class="card">--}}
-{{--            <div class="card-header">Objave</div>--}}
-
-{{--            <div class="card-body">--}}
-{{--                <form action="{{ route('homepage') }}" method="GET" class="form-group row mt-3">--}}
-{{--                    <div class="col-3">--}}
-{{--                        <input type="text" name="search" class="form-control" value="{{ $searchTerm }}"--}}
-{{--                               placeholder="Pretraga naslova">--}}
-{{--                    </div>--}}
-{{--                    <div class="col-3">--}}
-{{--                        <input id="tags-output" type="hidden" name="tags" value="{{ $tags }}" autocomplete="off">--}}
-{{--                        <input id="tags-input" type="text" class="tagify-single-line" value="{{ $tags }}" autocomplete="off"--}}
-{{--                               placeholder="Pretraga tagova" data-type="search">--}}
-{{--                        <div id="tags-autocomplete"></div>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-md-3">--}}
-{{--                        <select name="category" class="form-control @error('category') is-invalid @enderror">--}}
-{{--                            <option value="" @if ($category == '') selected @endif>Sve kategorije</option>--}}
-{{--                            @foreach ($categories as $item)--}}
-{{--                                <option value="{{ $item->id }}"--}}
-{{--                                        @if (old('category', $category) == $item->id) selected @endif>{{ $item->name }}</option>--}}
-{{--                            @endforeach--}}
-{{--                        </select>--}}
-
-{{--                        @error('category')--}}
-{{--                        <span class="invalid-feedback" role="alert">--}}
-{{--                                    <strong>{{ $message }}</strong>--}}
-{{--                                </span>--}}
-{{--                        @enderror--}}
-{{--                    </div>--}}
-{{--                    <div class="col-3">--}}
-{{--                        <button type="submit" class="btn btn-primary">Pretraži</button>--}}
-{{--                        @if(!empty($searchTerm))--}}
-{{--                            <a href="{{ route('admin.posts.index') }}" class="ml-3">Prikaži sve</a>--}}
-{{--                        @endif--}}
-{{--                    </div>--}}
-{{--                </form>--}}
-
-{{--                <div class="mt-4 justify-content-center">--}}
-{{--                    @foreach($posts as $post)--}}
-{{--                        <x-post :post="$post"/>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
-
-{{--                <div class="row mt-2 justify-content-center">--}}
-{{--                    {{ $posts->links() }}--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
     <script type="text/javascript">
         ALGOLIA_APP_ID = `<?php echo env('ALGOLIA_APP_ID', '') ?>`;
         ALGOLIA_PUBLIC_SECRET = `<?php echo env('ALGOLIA_PUBLIC_SECRET', '') ?>`;
