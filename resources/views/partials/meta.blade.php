933ea1f7228c7e565ecf60d66dc4917be83f41db
Makro Ilic <marko@weareneopix.com>
2020-04-24 11:38:55 +0200

    fix(manifest): add credential support

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -8,7 +8,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicons/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicons/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicons/favicon-16x16.png') }}">
-<link rel="manifest" href="{{ asset('/favicons/site.webmanifest') }}">
+<link rel="manifest" crossorigin="use-credentials" href="{{ asset('/favicons/site.webmanifest') }}">
 <link rel="mask-icon" href="{{ asset('/favicons/safari-pinned-tab.svg') }}" color="#000000'">
 <link rel="shortcut icon" href="{{ asset('/favicons/favicon.ico') }}">
 <meta name="msapplication-TileColor" content="#000000">
