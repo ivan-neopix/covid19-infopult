086aae8a494e236169700567ac389a9aec84c759
Ivan Stojanovic <ivan@weareneopix.com>
2020-04-26 14:55:33 +0200

    feat: remove dark mode and apply style for alert

--- resources/views/layouts/flash.blade.php
+++ resources/views/layouts/flash.blade.php
@@ -1,5 +1,5 @@
 @if (Session::has('success'))
-    <div class="mt-3">
+    <div class="container">
         <section class="update-info">
             <div class="updated-msg alert alert-success alert-dismissable" style="">
                 {{ Session::get('success') }}
@@ -10,7 +10,7 @@
 @endif
 
 @if (Session::has('error'))
-    <div class="mt-3">
+    <div class="container">
         <section class="update-info">
             <div class="updated-msg alert alert-danger alert-dismissable" style="">{{ Session::get('error') }}
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
@@ -21,7 +21,7 @@
 
 
 @if (Session::has('warning'))
-    <div class="mt-3">
+    <div class="container">
         <section class="update-info">
             <div class="updated-msg alert alert-warning alert-dismissable" style="">{{ Session::get('warning') }}
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
